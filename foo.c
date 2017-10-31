2354fc2cffb7f0587cb4437c10dd916c83efc7b5
Alex Lu <alexlu876@gmail.com>
2017-10-31 11:10:01 -0400

    sd

--- foo.c
+++ foo.c
@@ -8,9 +8,25 @@
 #include <stdlib.h>
 #include <errno.h>
 
+void error_check(int k){
+  if(!k){
+    printf("%s\n", strerror(errno));
+    exit(1);
+  }
+}
+
 int main(){
   struct stat buff;
   stat("bar", &buff);
+  int fd;
+  fd = open("bar", O_WRONLY | O_TRUNC, 0777);
+  int arr[5];
+  int i;
+  for(i = 0; i < 5; i++){
+    arr[i] = i;
+  }
+  error_check(write(fd, arr, 20));
+  close(fd);
   printf("sizeof file: %lu\n", buff.st_size);
   printf("permissions of file: %o\n", (buff.st_mode << 23) >> 23);
   printf("time of last access: %s", ctime(&buff.st_atime));
